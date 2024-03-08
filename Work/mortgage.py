21760bb0df9e0449f81919545dda3f23b3d2f123
Mu Ye <emailyemu@gmail.com>
2024-03-07 20:53:48 -0800

    ex1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,13 +1,18 @@
 principal = 500000.0
 rate = 0.05
 regular_payment = 2684.11
-extra_payment = regular_payment + 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 total_paid = 0.0
 month = 0
 
 while principal > 0:
     month += 1
-    payment = extra_payment if month <= 12 else regular_payment
+    if extra_payment_start_month <= month and month <= extra_payment_end_month:
+        payment = regular_payment + extra_payment
+    else:
+        payment = regular_payment
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
 
