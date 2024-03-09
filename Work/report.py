80be505e62a8a8f52208355818f781502f7310a4
Mu Ye <emailyemu@gmail.com>
2024-03-09 13:37:12 -0800

    ex2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,13 @@
-# report.py
-#
-# Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            shares = int(row[1])
+            price = float(row[2])
+            portfolio.append((row[0], shares, price))
+    return portfolio
