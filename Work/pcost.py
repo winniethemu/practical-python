8b7ce2f58ae7c2d9858c768ad50faeef96f721fe
Mu Ye <emailyemu@gmail.com>
2024-03-09 09:45:35 -0800

    ex1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
-# pcost.py
-#
-# Exercise 1.27
+with open('Data/portfolio.csv') as file:
+    line_num = 0
+    total_cost = 0
+    for line in file:
+        if line_num == 0:
+            line_num += 1
+            pass
+        else:
+            [name, shares, price] = line.split(',')
+            total_cost += int(shares) * float(price)
+    print(f'Total cost {total_cost}')
