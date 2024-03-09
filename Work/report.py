bf8469d3fccd69aca8672737035e51827d0c1497
Mu Ye <emailyemu@gmail.com>
2024-03-09 14:42:42 -0800

    ex2.7

--- Work/report.py
+++ Work/report.py
@@ -23,5 +23,22 @@ def read_prices(filename):
         for row in rows:
             if len(row) > 0:
                 name, price = row
-                prices[name] = price
+                prices[name] = float(price)
     return prices
+
+
+initial_value = 0
+current_value = 0
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+for holding in portfolio:
+    initial_price = holding['price']
+    initial_value += initial_price * holding['shares']
+    current_price = prices[holding['name']]
+    current_value += current_price * holding['shares']
+
+print('Initial value', initial_value)
+print('Current value', current_value)
+print('Gain', current_value - initial_value)
