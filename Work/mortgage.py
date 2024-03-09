747fa377c04d916768b5a55dc16c7937eb4fdf8a
Mu Ye <emailyemu@gmail.com>
2024-03-08 19:50:20 -0800

    ex1.17

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -19,7 +19,9 @@ while principal > 0:
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
-    print(month, round(total_paid, ndigits=2), round(principal, ndigits=2))
+    print(
+        f'{month} {round(total_paid, ndigits=2)} {round(principal, ndigits=2)}'
+    )
 
-print('Total payment', total_paid)
-print('Total month', month)
+print(f'Total payment {total_paid}')
+print(f'Total month {month}')
