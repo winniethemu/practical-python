bcc7bf4eab13bec3018b8a7619e7bfbb5874c3f2
Mu Ye <emailyemu@gmail.com>
2024-03-10 12:12:33 -0700

    ex3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,17 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+
+def parse_csv(filename):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+    return records
