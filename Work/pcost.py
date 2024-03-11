23e56e079e713f5a381578459e77c03b82da3813
Mu Ye <emailyemu@gmail.com>
2024-03-11 14:55:52 -0700

    ex3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,18 @@
 import sys
-import csv
+from report import read_portfolio
 
 
 def portfolio_cost(filename):
     total_cost = 0
-    with open(filename, 'rt') as file:
-        rows = csv.reader(file)
-        headers = next(rows)
-        for row_num, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                shares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += shares * price
-            except ValueError:
-                print(f'Row {row_num}: Malformatted row {row}')
+    portfolio = read_portfolio(filename)
+    for row_num, row in enumerate(portfolio, start=1):
+        try:
+            shares = int(row['shares'])
+            price = float(row['price'])
+            total_cost += shares * price
+        except ValueError:
+            print(f'Row {row_num}: Malformatted row {row}')
+
     return total_cost
 
 
