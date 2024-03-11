57c6839afa9ec58717109074a1037a6080ac2463
Mu Ye <emailyemu@gmail.com>
2024-03-11 12:18:29 -0700

    ex3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,12 +1,17 @@
 import csv
 
 
-def parse_csv(filename, select=False, types=None, has_headers=True):
+def parse_csv(
+        filename,
+        select=False,
+        types=None,
+        has_headers=True,
+        delimiter=','):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
         if has_headers:
             headers = next(rows)
         records = []
