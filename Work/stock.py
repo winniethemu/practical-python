19bd32a7ec587ff5d6ebbb30c006b129437a65c0
Mu Ye <emailyemu@gmail.com>
2024-03-12 11:07:25 -0700

    ex4.2

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,12 @@
 class Stock:
-    def __init__(self, name, shares, price):
+    def __init__(self, name: str, shares: int, price: float):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def cost(self) -> float:
+        return self.shares * self.price
+
+    def sell(self, num: int) -> None:
+        if num <= self.shares:
+            self.shares -= num
