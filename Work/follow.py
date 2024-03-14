607b360ac5809c3ac4eba182eca89f17e233dc4c
Mu Ye <emailyemu@gmail.com>
2024-03-14 12:42:14 -0700

    ex6.5

---
+++ Work/follow.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+f = open('Data/stocklog.csv')
+
+# Move file pointer 0 bytes from end of file
+f.seek(0, os.SEEK_END)
+
+while True:
+    line = f.readline()
+    if line == '':
+        time.sleep(0.1)
+        continue
+    fields = line.split(',')
+    name = fields[0].strip('"')
+    price = float(fields[1])
+    change = float(fields[4])
+    if (change < 0):
+        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
