4fa58d8388d29c105272c32f3d616dacb5db608f
Mu Ye <emailyemu@gmail.com>
2024-03-14 13:39:36 -0700

    ex6.7

--- Work/follow.py
+++ Work/follow.py
@@ -13,10 +13,12 @@ def follow(filename):
 
 
 if __name__ == '__main__':
+    import report
+    portfolio = report.read_portfolio('Data/portfolio.csv')
     for line in follow('Data/stocklog.csv'):
         fields = line.split(',')
         name = fields[0].strip('"')
         price = float(fields[1])
         change = float(fields[4])
-        if (change < 0):
+        if name in portfolio:
             print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
