5281f9baa3f8b119ec578eafae8d6847523a734d
Mu Ye <emailyemu@gmail.com>
2024-03-14 13:36:57 -0700

    ex6.6

--- Work/follow.py
+++ Work/follow.py
@@ -1,19 +1,22 @@
 import os
-import time
 
-f = open('Data/stocklog.csv')
 
-# Move file pointer 0 bytes from end of file
-f.seek(0, os.SEEK_END)
+def follow(filename):
+    with open(filename) as f:
+        # Move file pointer 0 bytes from end of file
+        f.seek(0, os.SEEK_END)
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)
-        continue
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if (change < 0):
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+        while True:
+            line = f.readline()
+            if len(line) > 0:
+                yield line
+
+
+if __name__ == '__main__':
+    for line in follow('Data/stocklog.csv'):
+        fields = line.split(',')
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if (change < 0):
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
