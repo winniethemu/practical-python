3ab5d703436ddbe0fbcb9c01b8c812a69918f14b
Mu Ye <emailyemu@gmail.com>
2024-03-14 15:41:57 -0700

    ex6.11

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,5 +1,7 @@
-from follow import follow
 import csv
+
+from follow import follow
+from report import read_portfolio
 
 
 def convert_types(rows, types):
@@ -10,6 +12,12 @@ def convert_types(rows, types):
 def make_dicts(rows, headers):
     for row in rows:
         yield dict(zip(headers, row))
+
+
+def filter_symbols(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
 
 
 def select_columns(rows, indices):
@@ -27,6 +35,8 @@ def parse_stock_data(lines):
 
 if __name__ == '__main__':
     lines = follow('Data/stocklog.csv')
+    portfolio = read_portfolio('Data/portfolio.csv')
     rows = parse_stock_data(lines)
+    rows = filter_symbols(rows, portfolio)
     for row in rows:
         print(row)
