a2e4638ebee779f04ee6b815553030fd569402ea
Mu Ye <emailyemu@gmail.com>
2024-03-21 23:12:14 -0700

    ex9.5

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Your Name",
+    author_email="you@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
